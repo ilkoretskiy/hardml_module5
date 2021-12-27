7869be482b3d12e71f3216596d1a7bf240fe26e0
Ilya Koretskiy <ilkor@mobalyticshq.com>
2021-12-27 17:01:16 +0400

    fix: added missed env variables in run.sh

--- lesson2/run.sh
+++ lesson2/run.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+export REDIS_HOST=62.84.127.188
 export SERVICE_PORT=12100
+export SERVICE_HOST=62.84.125.150
 
 docker run --rm -d  -p ${SERVICE_PORT}:8000 \
+    --env REDIS_HOST=${REDIS_HOST} \
+    --env SERVICE_HOST=${SERVICE_HOST} \
+    --env SERVICE_PORT=${SERVICE_PORT} \
     --name hardml5_lesson2 \
     hardml5_lesson2 
 
